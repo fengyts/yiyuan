4569f5025586db69205ee2eb41f3fdacae3bc5da
fengyuntao <fengyuntao@jiaxinmore.com>
2016-10-28 17:34:52 +0800

    junit test

--- bayue/parent/backend/backend-front/src/main/java/ng/bayue/backend/controller/TestController.java
+++ bayue/parent/backend/backend-front/src/main/java/ng/bayue/backend/controller/TestController.java
@@ -3,7 +3,9 @@ package ng.bayue.backend.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping({"/test"})
@@ -27,9 +29,12 @@ public class TestController {
 		return "/appfont/home";
 	}
 	
-	@RequestMapping({"/test"})
-	public void testdfdfd(){
+	@RequestMapping(value={"/testContent"},produces={"application/json"})
+	public void testdfdfd(String level,Integer pageNo){
+		System.out.println(level);
+		System.out.println(pageNo);
 		System.out.println("maxSize:"+maxSize);
 	}
+	
 	
 }
